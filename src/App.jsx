09487f8bebd25247194f44e4f08740d050e3e701
kapil9861 <aaryalkkapil@gmail.com>
2023-08-28 23:11:12 +0545

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 
 import "./App.css";
 import React from "react";
@@ -7,25 +7,34 @@ import LandingPage from "./Homepage/LandingPage";
 import AboutPage from "./AboutUspage/Components/AboutPage";
 import MeetOurTeam from "./ManagmentTeamPage/Components/MeetOurTeam";
 import Header from "./Header/Header.jsx";
-import {Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ImageSlider from "./ImageSlider/ImageSlider.jsx";
 import Footer from "./Footer/Footer.jsx";
+import LoginPage from "./login/pages/Login";
 
 export default function App() {
-    return (
-
-        <div className="App">
-            {/*<>*/}
-            {/*    <Routes>*/}
-            {/*        <Route path="/HomePage" element={{""}}/>*/}
-            {/*    </Routes>*/}
-            {/*</>*/}
-            {/*<Header/>*/}
-            {/*<ImageSlider/>*/}
-            <LandingPage/>
-            <Footer/>
-            {/*<AboutPage/>*/}
-            {/*<MeetOurTeam/>*/}
-        </div>
-    );
+  return (
+    <>
+      <div className="App">
+        {/*<>*/}
+        {/*    <Routes>*/}
+        {/*        <Route path="/HomePage" element={{""}}/>*/}
+        {/*    </Routes>*/}
+        {/*</>*/}
+        {/*<Header/>*/}
+        {/*<ImageSlider/>*/}
+        <LandingPage />
+        <Footer />
+        {/*<AboutPage/>*/}
+        {/*<MeetOurTeam/>*/}
+      </div>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 }
--- src/login/pages/Login.js
+++ src/login/pages/Login.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import Header from "./Header/Header.jsx";
 export default function LoginPage() {
-  return <></>;
+  return <>
+  <Header/>
+  </>;
 }
