788cc77e960292b877dd85fd4d98626f6635037f
Sarthak <sarthakstha24@gmail.com>
2023-08-30 22:12:42 +0545

    Added New Login Popup

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,38 @@ import "./App.css";
 import React from "react";
 import {Route, Routes} from "react-router-dom";
 
-import LandingPage from "./Homepage/LandingPage.jsx";
-import MeetOurTeamPage from "./ManagementTeamPage/Components/MeetOurTeam.jsx"
-import AboutUsPage from "./AboutUspage/Components/AboutPage.jsx";
-import RegistrationPage from "./Registration/Registration.jsx";
-import Login from "./login/components/Login.jsx";
-import LoginPage from "./login/pages/LoginPage.jsx";
+import LandingPage from "./Homepage/LandingPage";
+import MeetOurTeamPage from "./ManagementTeamPage/Components/MeetOurTeam.jsx";
+import AboutUsPage from "./AboutUspage/Components/AboutPage";
+import RegistrationPage from "./Registration/Registration";
+import LoginPageNewVersion from "./LoginPage/LoginPageNewVersion";
 
 export default function App() {
     return (<div className="App">
-        <>
-            <Routes>
-                <Route path="/" element={<LandingPage/>}/>
-                <Route path="/ManagementTeam" element={<MeetOurTeamPage/>}/>
-                <Route path="/AboutUsPage" element={<AboutUsPage/>}/>
-                <Route path="/LoginPage" element={<LoginPage/>}/>
-                <Route path="/Registration" element={<RegistrationPage/>}/>
-                <Route path="*" element={<h1>PageNotFound!</h1>}/>
-            </Routes>
-        </>
-    </div>);
+            <>
+                <Routes>
+                    <Route path="/" element={<LandingPage/>}>
+                        <Route path="/Login" element={<LoginPageNewVersion/>}/>
+                    </Route>
+
+                    <Route path="/ManagementTeam" element={<MeetOurTeamPage/>}>
+                        <Route
+                            path="/ManagementTeam/Login"
+                            element={<LoginPageNewVersion/>}
+                        />
+                    </Route>
+
+                    <Route path="/AboutUsPage" element={<AboutUsPage/>}>
+                        <Route
+                            path="/AboutUsPage/Login"
+                            element={<LoginPageNewVersion/>}
+                        />
+                    </Route>
+
+                    <Route path="/Registration" element={<RegistrationPage/>}/>
+
+                    <Route path="*" element={<h1>PageNotFound!</h1>}/>
+                </Routes>
+            </>
+        </div>);
 }
