6297541d5ec79515e6d1c4958c43f9424257ac1d
birajan <birajanrb@gmail.com>
2023-09-01 23:26:03 +0545

    Added axios request interceptor

---
+++ src/Completed_Login/LoginPage/LoginPageNewVersion.jsx
@@ -0,0 +1,9 @@
+import LoginComponent from "./components/LoginComponent";
+
+export default function LoginPageNewVersion() {
+  return (
+    <>
+      <LoginComponent />
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+
+//initial localStorage value set to empty string
+localStorage.setItem("BearerToken", JSON.stringify(""));
+
+//axios request interceptor
+axios.interceptors.request.use((request) => {
+  const localStorageBT_Value = JSON.parse(localStorage.getItem("BearerToken"));
+  request.headers["Authorization"] = "Bearer" + localStorageBT_Value;
+  return request;
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
