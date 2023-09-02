9b185896c92eaed7496dc63965608c2f604e06a8
Sarthak <sarthakstha24@gmail.com>
2023-09-02 22:01:23 +0545

    Directory changes

--- src/Services/Components/ServicesPage.jsx
+++ src/Services/Components/ServicesPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import AboutPicture from "./pictures/6237073.jpg";
-import Establishment from "./pictures/People talking with managers in bank flat vector illustration.jpg";
+import Establishment from "./pictures/PeopleTalking.jpg";
 import InsurancePicture from "./pictures/26675.jpg";
 import AgriculturePicture from "./pictures/8400.jpg";
 import "./ServicesPage.css";
@@ -11,15 +11,16 @@ export default function ServicesPage() {
             <div className="about-section">
                 <div className="about-content">
                     <div className="about-image">
-                        <img src={AboutPicture} alt="Image"/>
+                        <img src={AboutPicture} alt="Image" />
                     </div>
                     <div className="about-description">
                         <h2>Remittance Services</h2>
                         <p>
-                            Sending money to loved ones far away or receiving funds from abroad is easy with JD Bank.
-                            Our secure and quick remittance services ensure smooth transactions. You can trust us for
-                            competitive exchange rates and a user-friendly experience, connecting you to your global
-                            financial needs.
+                            Sending money to loved ones far away or receiving funds from
+                            abroad is easy with JD Bank. Our secure and quick remittance
+                            services ensure smooth transactions. You can trust us for
+                            competitive exchange rates and a user-friendly experience,
+                            connecting you to your global financial needs.
                         </p>
                     </div>
                 </div>
@@ -28,28 +29,30 @@ export default function ServicesPage() {
                         <div className="establishment-description">
                             <h2>Utility Services</h2>
                             <p>
-                                Life's simpler with JD Bank's utility services. Paying bills is convenient and
-                                straightforward. From electricity and water bills to internet and phone payments, we
-                                make it easy to manage your expenses. Enjoy the ease of handling your utilities all in
-                                one place with JD Bank.
+                                Life's simpler with JD Bank's utility services. Paying bills is
+                                convenient and straightforward. From electricity and water bills
+                                to internet and phone payments, we make it easy to manage your
+                                expenses. Enjoy the ease of handling your utilities all in one
+                                place with JD Bank.
                             </p>
                         </div>
                         <div className="establishment-image">
-                            <img src={Establishment} alt="Image"/>
+                            <img src={Establishment} alt="Image" />
                         </div>
                     </div>
                 </div>
                 <div className="about-content">
                     <div className="about-image">
-                        <img src={InsurancePicture} alt="Image"/>
+                        <img src={InsurancePicture} alt="Image" />
                     </div>
                     <div className="about-description">
-                        <h2>Insurance Services</h2>
+                        <h2>Insurance Services:</h2>
                         <p>
-                            Your peace of mind matters. JD Bank offers insurance plans to protect what's important. Our
-                            coverage includes health, life, and property, providing security against the unexpected.
-                            With flexible options and expert advice, JD Bank helps you make wise choices for a secure
-                            future.
+                            Your peace of mind matters. JD Bank offers insurance plans to
+                            protect what's important. Our coverage includes health, life, and
+                            property, providing security against the unexpected. With flexible
+                            options and expert advice, JD Bank helps you make wise choices for
+                            a secure future.
                         </p>
                     </div>
                 </div>
@@ -58,14 +61,15 @@ export default function ServicesPage() {
                         <div className="establishment-description">
                             <h2>Agriculture Services</h2>
                             <p>
-                                For farmers and those in agriculture, JD Bank has specialized solutions to support your
-                                growth. Whether you need funding for crops, equipment, or agribusiness, we're here for
-                                you. Our team understands your unique needs and is ready to help you succeed in your
-                                agricultural ventures.
+                                For farmers and those in agriculture, JD Bank has specialized
+                                solutions to support your growth. Whether you need funding for
+                                crops, equipment, or agribusiness, we're here for you. Our team
+                                understands your unique needs and is ready to help you succeed
+                                in your agricultural ventures.
                             </p>
                         </div>
                         <div className="establishment-image">
-                            <img src={AgriculturePicture} alt="Image"/>
+                            <img src={AgriculturePicture} alt="Image" />
                         </div>
                     </div>
                 </div>
