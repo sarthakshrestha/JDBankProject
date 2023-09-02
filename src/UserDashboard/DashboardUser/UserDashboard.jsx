6f77c37bcb2fc9e8a5a34666486fe257a8585e7f
Sarthak <sarthakstha24@gmail.com>
2023-09-02 23:19:06 +0545

    Admin Dashboard retrieval and Updating of Data Added.

--- src/UserDashboard/DashboardUser/UserDashboard.jsx
+++ src/UserDashboard/DashboardUser/UserDashboard.jsx
@@ -1,58 +1,85 @@
-import React from "react";
-import SideBar from "../SideBar/SideBar";
+import React, {useState, useEffect} from "react";
+import Sidebar from "../Sidebar/Sidebar";
+import axios from "axios";
 import "./userdashboard.css";
 
 function UserDashboard() {
-  return (
-    <div>
-      <SideBar />
-      <div class="dashboard">
-        <h1>
-          Welcome, <span id="user-name">John Doe</span>!
-        </h1>
-        <div class="user-info">
-          <div class="balance">
-            <h2>
-              <strong>Available Balance</strong>
-            </h2>
-            <p id="balance-amount">Rs 10,000.00</p>
-          </div>
-          <div class="user-details">
-            <div class="user-id">
-              <label for="user-id">
-                <strong>User ID</strong>
-              </label>
-              <p id="user-id">12345</p>
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const apiUrl = "http://localhost:8080/user/1"; // Replace with your API endpoint
+
+        axios
+            .get(apiUrl)
+            .then((response) => {
+                setUserData(response.data); // Assuming the API returns user data
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error("Error fetching user data:", error);
+                setLoading(false);
+            });
+    }, []);
+
+    return (
+        <div>
+            <Sidebar/>
+            <div className="dashboard">
+                <h1>
+                    Welcome,{" "}
+                    <span id="user-name">
+            {userData ? `${userData.firstName} ${userData.lastName}` : "Loading..."}
+          </span>
+                    !
+                </h1>
+                <div className="user-info">
+                    <div className="balance">
+                        <h2>
+                            <strong>Available Balance</strong>
+                        </h2>
+                        <p id="balance-amount">
+                            {userData ? `Rs ${userData.account.balance.toFixed(2)}` : "Loading..."}
+                        </p>
+                    </div>
+                    <div className="user-details">
+                        <div className="user-id">
+                            <label htmlFor="user-id">
+                                <strong>User ID</strong>
+                            </label>
+                            <p id="user-id">{userData ? userData.user_id : "Loading..."}</p>
+                        </div>
+                        <div className="user-fullname">
+                            <label htmlFor="full-name">
+                                <strong>Full Name</strong>
+                            </label>
+                            <p id="full-name">
+                                {userData ? `${userData.firstName} ${userData.lastName}` : "Loading..."}
+                            </p>
+                        </div>
+                        <div className="user-email">
+                            <label htmlFor="email">
+                                <strong>Email</strong>
+                            </label>
+                            <p id="email">{userData ? userData.email : "Loading..."}</p>
+                        </div>
+                        <div className="user-phone">
+                            <label htmlFor="phone-number">
+                                <strong>Phone Number</strong>
+                            </label>
+                            <p id="phone-number">{userData ? userData.phoneNumber : "Loading..."}</p>
+                        </div>
+                        <div className="user-address">
+                            <label htmlFor="address">
+                                <strong>Address</strong>
+                            </label>
+                            <p id="address">{userData ? userData.address : "Loading..."}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="user-fullname">
-              <label for="full-name">
-                <strong>Full Name</strong>
-              </label>
-              <p id="full-name">John Doe</p>
-            </div>
-            <div class="user-email">
-              <label for="email">
-                <strong>Email</strong>
-              </label>
-              <p id="email">johndoe@example.com</p>
-            </div>
-            <div class="user-phone">
-              <label for="phone-number">
-                <strong>Phone Number</strong>
-              </label>
-              <p id="phone-number">123-456-7890</p>
-            </div>
-            <div class="user-address">
-              <label for="address">
-                <strong>Address</strong>
-              </label>
-              <p id="address">123 Main St, City</p>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default UserDashboard;
