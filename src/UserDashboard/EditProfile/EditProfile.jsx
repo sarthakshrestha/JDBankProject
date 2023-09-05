c8011b931fc115a8f7c0caf8fe95ddfb66221985
birajan <birajanrb@gmail.com>
2023-09-05 23:39:54 +0545

    Upload test

--- src/UserDashboard/EditProfile/EditProfile.jsx
+++ src/UserDashboard/EditProfile/EditProfile.jsx
@@ -82,21 +82,22 @@ function EditProfile() {
     // axios.get(url).then((res) => {
     //   userImage.current.src = res.data;});
 
-    // axios
-    // .get(url, { responseType: "arraybuffer",})
-    // .then((response) => {
-    //   userImage.current.src =  Buffer.from(response.data, "binary").toString("base64")});
-    // }
-
-    axios.get(url, { responseType: "arraybuffer" }).then((response) => {
-      const imageBase64 = btoa(
-        new Uint8Array(response.data).reduce(
-          (data, byte) => data + String.fromCharCode(byte),
-          ""
-        )
-      );
+    axios
+    .get(url, { responseType: "arraybuffer",})
+    .then((response) => {
+      imageBase64 =  Buffer.from(response.data, "binary").toString("base64")});
       userImage.current.src = `data:image/jpeg;base64,${imageBase64}`;
-    });
+    }
+
+    // axios.get(url, { responseType: "arraybuffer" }).then((response) => {
+    //   const imageBase64 = btoa(
+    //     new Uint8Array(response.data).reduce(
+    //       (data, byte) => data + String.fromCharCode(byte),
+    //       ""
+    //     )
+    //   );
+    //   userImage.current.src = `data:image/jpeg;base64,${imageBase64}`;
+    // });
   }
 
   const handleInputChange = (event) => {
