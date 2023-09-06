7d7f4ab0656505ea1bfbf11262ccb5826edaa1af
birajan <birajanrb@gmail.com>
2023-09-06 09:14:41 +0545

    changed header and footer & img 2

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,101 +1,78 @@
 import "./Footer.css";
 import logo from "./Images/Logo.png";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Footer() {
     return (
-        <>
-            <div className="footer">
-                <div className="footerLeft">
-                    <img src={logo} alt="Logo"/>
-                    <div className="footer_BankName">JD Bank.co</div>
-                </div>
+        <footer className="footer">
+            <div className="footerLeft">
+                <img src={logo} alt="Logo" className="footerLogo" />
+                <div className="footer_BankName">JD Bank.co</div>
+            </div>
 
-                <div className="footerRight">
-                    <div className="footerRightTop">
-                        <div className="JDBankCustomerSupport">
-                            <h4>
-                                JDBank
-                                <br/>
-                                CustomerSupport
-                            </h4>
-
-                            <ul>
-                                <li>
-                                    <NavLink to="/Loan Services">jdbank@support.com.np</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="">+01-56969696</NavLink>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div className="services">
-                            <h4>Services</h4>
-                            <ul>
-                                <li>
-                                    <NavLink to="/Loan Services">Loan Services</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/ATMLocations">ATM Locations</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/Headquaters">Headquaters</NavLink>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div className="aboutUs">
-                            <h4>About Us</h4>
-                            <ul>
-                                <li>
-                                    <NavLink to="/HistoryOfJDBank">History of JDBank</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/ManagementTeam">JD Bank - Team</NavLink>
-                                </li>
-                            </ul>
-                        </div>
+            <div className="footerRight">
+                <div className="footerRightTop">
+                    <div className="JDBankCustomerSupport">
+                        <h4>Customer</h4>
+                        <h4>Support</h4>
+                        <ul>
+                            <li>
+                                <NavLink to="/Loan Services">jdbank@support.com.np</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="">+01-56969696</NavLink>
+                            </li>
+                        </ul>
                     </div>
 
-                    <div className="footerRightBottom">
-                        <div className="socialMedia">
-                            <h4>Follow Us</h4>
+                    <div className="services">
+                        <h4>Services</h4>
+                        <ul>
+                            <li>
+                                <NavLink to="/Loan Services">Loan Services</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/ATMLocations">ATM Locations</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/Headquaters">Headquarters</NavLink>
+                            </li>
+                        </ul>
+                    </div>
 
-                            <section id="lab_social_icon_footer">
-                                <div class="social-container">
-                                    <div class="social-icons">
-                                        <a href="https://www.facebook.com" target="_blank">
-                                            <i
-                                                id="social-fb"
-                                                class="fa fa-facebook-square fa-3x social"
-                                            ></i>
-                                        </a>
-                                        <a href="https://www.twitter.com" target="_blank">
-                                            <i
-                                                id="social-tw"
-                                                class="fa fa-twitter-square fa-3x social"
-                                            ></i>
-                                        </a>
-                                        <a href="https://www.gmail.com" target="_blank">
-                                            <i
-                                                id="social-gp"
-                                                class="fa fa-google-plus-square fa-3x social"
-                                            ></i>
-                                        </a>
-                                        <a href="https://mailto:#" target="_blank">
-                                            <i
-                                                id="social-em"
-                                                class="fa fa-envelope-square fa-3x social"
-                                            ></i>
-                                        </a>
-                                    </div>
-                                </div>
-                            </section>
+                    <div className="aboutUs">
+                        <h4>About Us</h4>
+                        <ul>
+                            <li>
+                                <NavLink to="/HistoryOfJDBank">History of JDBank</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/ManagementTeam">JD Bank - Team</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="footerRightBottom">
+                    <div className="socialMedia">
+                        <h4>Follow Us</h4>
+                        <div class="social-icons">
+                            <a href="https://www.facebook.com" target="_blank">
+                                <i id="social-fb" class="fa fa-facebook-square fa-2x social"></i>
+                            </a>
+                            <a href="https://www.twitter.com" target="_blank">
+                                <i id="social-tw" class="fa fa-twitter-square fa-2x social"></i>
+                            </a>
+                            <a href="https://www.gmail.com" target="_blank">
+                                <i id="social-gp" class="fa fa-google-plus-square fa-2x social"></i>
+                            </a>
+                            <a href="mailto:" target="_blank">
+                                <i id="social-em" class="fa fa-envelope-square fa-2x social"></i>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
+        </footer>
     );
 }
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -26,65 +26,47 @@ export default function Header() {
       accoutView.current = false;
     } else {
       currentLoc.pathname == "/"
-        ? navTo("/Login")
-        : navTo(currentLoc.pathname + "/Login");
+          ? navTo("/Login")
+          : navTo(currentLoc.pathname + "/Login");
       accoutView.current = true;
     }
   }
 
   return (
-    <div className="header">
-      <div className="headerUpper">
-        <Link to="/" className="headerUpper_Logo">
-          <img src={logo} alt="Logo" />
-        </Link>
+      <div className="header">
+        <div className="headerUpper">
+          <Link to="/" className="headerUpper_Logo">
+            <img src={logo} alt="Logo" />
+          </Link>
 
-        <div className="headerUpper_BankName">JD Bank.co</div>
+          <div className="headerUpper_BankName">JD Bank.co</div>
 
-        <button
-          className="headerUpper_AccountButton"
-          onClick={accoutButtonHandler}
-        >
-          Account
-        </button>
-        <Outlet />
-      </div>
-
-      <div className="headerLower">
-        <ul className="headerLower_Nav">
-          <li>
-            <NavLink to="/Services">Services</NavLink>
-          </li>
-          <li>
-            <NavLink to="/ManagementTeam">Management Team</NavLink>
-          </li>
-          <li>
-            <NavLink to="/AboutUsPage">About Us</NavLink>
-          </li>
-          <li>
-            <NavLink to="/FAQ">FAQ</NavLink>
-          </li>
-        </ul>
-
-        <div className="headerLower_SearchBox_containter">
-          <input
-            className="headerLower_SearchBox"
-            type="search"
-            onChange={searchHandle}
-            placeholder="Search..."
-          />
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
+          <button
+              className="headerUpper_AccountButton"
+              onClick={accoutButtonHandler}
           >
-            <path d="M15.853 16.56c-1.683 1.517-3.911 2.44-6.353 2.44-5.243 0-9.5-4.257-9.5-9.5s4.257-9.5 9.5-9.5 9.5 4.257 9.5 9.5c0 2.442-.923 4.67-2.44 6.353l7.44 7.44-.707.707-7.44-7.44zm-6.353-15.56c4.691 0 8.5 3.809 8.5 8.5s-3.809 8.5-8.5 8.5-8.5-3.809-8.5-8.5 3.809-8.5 8.5-8.5z" />
-          </svg>
+            Account
+          </button>
+          <Outlet />
         </div>
+
+        <div className="headerLower">
+          <ul className="headerLower_Nav">
+            <li>
+              <NavLink to="/Services">Services</NavLink>
+            </li>
+            {/*<li>*/}
+            {/*  <NavLink to="/ManagementTeam">Management Team</NavLink>*/}
+            {/*</li>*/}
+            <li>
+              <NavLink to="/AboutUsPage">About Us</NavLink>
+            </li>
+            <li>
+              <NavLink to="/FAQ">FAQ</NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="headerDivider"></div>
       </div>
-      <div className="headerDivider"></div>
-    </div>
   );
 }
